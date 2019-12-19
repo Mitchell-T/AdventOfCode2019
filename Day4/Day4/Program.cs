dcf56c804be492a3b1548b31486ac326a02a4289
Mitchell Tuinstra <mitchelltuinstra2000@gmail.com>
2019-12-19 12:36:50 +0100

    start day 4

---
+++ Day4/Day4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> combinations = new List<int>();
+            for(int i = 264360; i < 746325; i++)
+            {
+
+            }
+        }
+    }
+}
